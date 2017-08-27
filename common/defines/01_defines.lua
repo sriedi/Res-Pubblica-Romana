3659e1eab43d0386788d9a42fd26e19cf276fc03
Sebastian Riedi <sebi.riedi@gmail.com>
2017-08-27 20:14:06 +0200

    Initial

---
+++ common/defines/01_defines.lua
@@ -0,0 +1,7 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+NDefines.NDiplomacy.PAGAN_PEACE_MONTHS = 48
+NDefines.NCharacter.MERCHANT_REPUBLIC_MAX_PATRICIANS = 6
+NDefines.NEconomy.MERCHANT_REPUBLIC_SALARY_MULT = 1
+NDefines.NEconomy.PATRICIAN_FAMILY_SHARES_REST = 3
---
+++ common/defines/dummy.lua
@@ -0,0 +1,12 @@
+NDefines = {
+	NDiplomacy = {
+		PAGAN_PEACE_MONTHS = 48
+	}
+	NCharacter={
+	MERCHANT_REPUBLIC_MAX_PATRICIANS = 6
+	}
+	NEconomy={
+	MERCHANT_REPUBLIC_SALARY_MULT = 1
+	PATRICIAN_FAMILY_SHARES_REST = 3
+	}
+}
